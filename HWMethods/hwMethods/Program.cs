88fbc12821f975559fd4eed0eed1fc20780b4736
Mirsamir B. Azimzada <mirsamirba@code.local>
2022-06-20 17:36:59 +0400

    HwMethodsAll

---
+++ HWMethods/hwMethods/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+
+namespace HwMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1) Verilmiş n -ədədinin 3 - ə və 7 - ə bölünüb - bölünməməsini tapın.
+            //HwMethods(21);
+
+            //2) n və m ədədləri verilir.Əgər n və m ədədləri hər ikisi cütdürsə n və m ədədlərinin cəmini hesablayın.
+            //HwMethods2(16,84);
+
+
+            //3) Verilmis n ve m(n<m) ededleri arasindaki tek ededlerin cemini tapin.
+
+            //HwMethods3(3, 49);
+
+
+            //4) Verilmish arrayin icindeki tek ededlerin cemini tapin.
+
+
+            // int[] arr = { 1, 2, 4, 7, 9, 11, 14, 16, 17, 19, 21, 24, 28, 31, 33 };
+            //HwMethods4(arr);
+
+
+
+            //5) Verilmish arrayin icindeki cut ededlerin sayini tapin.
+
+
+            //int[] arr = { 1, 2, 4, 7, 9, 11, 14, 16, 17, 19, 21, 24, 28, 31, 33 };
+            //HwMethods5(arr);
+
+
+            //    6) Verilmis n tam ededinin sade ve ya murekkeb oldugunu tapin.
+
+
+            //HwMethods6(25);
+
+
+        }
+
+
+
+        //1) Verilmiş n - ədədinin 3-ə və 7-ə bölünüb-bölünməməsini tapın.
+        //    static void HwMethods(int n)
+        // {if (n%3==0 && n%7==0)
+        //        Console.WriteLine("Divided");
+        //}
+
+
+        //2) n və m ədədləri verilir.Əgər n və m ədədləri hər ikisi cütdürsə n və m ədədlərinin cəmini hesablayın.
+
+        //static void HwMethods2(int n, int m)
+        // {
+        //     if (n % 2 == 0 && m % 2 == 0)
+        //         Console.WriteLine(n+m);
+        // }
+
+        //3) Verilmis n ve m (n<m) ededleri arasindaki tek ededlerin cemini tapin.
+
+        //static void HwMethods3(int n, int m)
+        //{
+        //    int sum = 0;
+        //    for (int i = n; i < m; i++)
+        //    {
+        //        if (i % 2 == 1 && i > n)
+        //            sum += i;
+
+        //    }
+        //    Console.WriteLine(sum);
+        //}
+
+
+
+
+
+        //4) Verilmish arrayin icindeki tek ededlerin cemini tapin.
+        //static void HwMethods4(int[] arr)
+        //{
+        //    int sum = 0;
+        //        for (int i = 0; i < arr.Length; i++)
+        //        {
+        //            if (arr[i] % 2 == 1) 
+        //            sum = sum + arr[i];
+        //        }
+
+        //    Console.WriteLine(sum);
+        //}
+
+
+
+        //5) Verilmish arrayin icindeki cut ededlerin sayini tapin.
+
+        //static void HwMethods5(int[] arr)
+        //{
+        //    int count = 0;
+        //    for (int i = 0; i < arr.Length; i++)
+        //    {
+        //        if (arr[i] % 2 == 0)
+        //             count++;
+        //    }
+
+        //    Console.WriteLine(count);
+        //}
+
+
+        //    6) Verilmis n tam ededinin sade ve ya murekkeb oldugunu tapin.
+        //static void HwMethods6(int n)
+        //{
+        //    int count = 0;
+        //    for (int i = 1; i <= n / 2; i++)
+        //    {
+        //        if (n%i == 0)
+        //        {
+        //            count++;
+        //        }
+        //    }
+        //    if (count > 1)
+        //    {
+        //        Console.WriteLine("composite");
+        //    }
+        //    else
+        //        Console.WriteLine("prime");
+        //}
+         
+            
+			
+    }
+
+}
